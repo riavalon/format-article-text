b225ad42a51c7eef551519a5786eba6acb6743bd
Raven Avalon <raven.avalon@rangle.io>
2017-02-14 14:16:18 -0500

    Clone content of selected element and paste inside a modal div

--- src/events/select-element.ext.js
+++ src/events/select-element.ext.js
@@ -1,8 +1,10 @@
 // Set document event listeners for selecting elements in page.
 ;(function() {
   // Default css.
+  // TODO: Find a way to populate this object dynamically from the extensions settings page.
   const DEFAULT_CSS = {
     width: '800px',
+    margin: '0 auto',
     fontFamily: '"Roboto", "Helvetica Neue", "Helvetica", "Calibri", "Arial", sans-serif',
     fontSize: '18px',
     lineHeight: '1.6',
@@ -20,7 +22,6 @@
 
   const addCSSToElement = (el) => {
     el.style.outline = '3px solid black';
-    console.log('add css called!');
   };
 
   const removeCSSFromElement = (el) => {
@@ -42,13 +43,81 @@
     styleKeys.forEach((key) => {
       el.style[key] = DEFAULT_CSS[key];
     });
+  };
+
+  const createCloseModalButton = () => {
+    const button = document.createElement('button');
+    button.setAttribute('id', 'FATModalCloseButton');
+    button.textContent = 'Close Modal';
+    document
+      .getElementById('FATModal')
+      .appendChild(button);
+  };
+
+  const createModalDiv = () => {
+    const modalDiv = document.createElement('div');
+    modalDiv.setAttribute('id', 'FATModal');
+    const styles = {
+      position: 'fixed',
+      zIndex: '9999',
+      top: '20px',
+      right: '20px',
+      left: '20px',
+      bottom: '10px',
+      boxShadow: '5px 5px 0 rgba(0,0,0,0.4)',
+      background: '#fbfbfb',
+      padding: '20px',
+      overflowY: 'scroll',
+      border: '1px solid rgba(0,0,0,0.2)'
+    };
+    const styleKeys = Object.keys(styles);
+    styleKeys.forEach((key) => {
+      modalDiv.style[key] = styles[key];
+    });
+    document.body.appendChild(modalDiv);
   }
 
-  const unsubscribeOnClick = (event) => {
-    applyStyles(event.target);
+  const removeModalDivFromPage = () => {
+    const modal = document.getElementById('FATModal');
+    const closeButton = document.getElementById('FATModalCloseButton');
+    closeButton.removeEventListener('click', removeModalDivFromPage);
+    document.body.removeChild(modal);
+  };
 
+  const cloneElement = (el) => {
+    if (el.tagName === 'BODY') {
+      let newEl = document.createElement('div');
+      // create a div as the container instead.
+      el.childNodes.forEach((node) => {
+        if (node.tagName && node.tagName !== 'SCRIPT') {
+          let clone = node.cloneNode(true);
+          newEl.appendChild(clone);
+        }
+      })
+      document.body.appendChild(newEl);
+      el = newEl;
+    } else {
+      el = el.cloneNode(true);
+    }
+
+    createModalDiv();
+    createCloseModalButton();
+    const modalDiv = document.getElementById('FATModal');
+    const modalDivCloseButton = document.getElementById('FATModalCloseButton');
+
+    modalDiv.appendChild(el);
+    removeCSSFromElement(el);
+    applyStyles(el);
+
+    // bind event listener to remove div from page.
+    modalDivCloseButton.addEventListener('click', removeModalDivFromPage);
+  };
+
+  const unsubscribeOnClick = (event) => {
     document.removeEventListener('mouseover', listenToEvents);
     document.removeEventListener('click', unsubscribeOnClick);
+
+    cloneElement(event.target);
   };
 
   const bindEventListeners = () => {
